733a88d58c1bfd18a07a7ad205451f21928358ee
henvisi-1994 <henvisi1994@gmail.com>
2025-10-16 17:29:56 -0500

    Inicio de proyecto

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),providePrimeNG({
+            theme: {
+                preset: Aura
+            }
+        })]
 };
